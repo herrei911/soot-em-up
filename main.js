806bf4b0f3092651fb11b7d7b29175063c7f26b3
Theraot <Theraot@gmail.com>
2020-02-09 17:18:44 -0500

    Multiples platillos voladores

--- main.js
+++ main.js
@@ -1,8 +1,21 @@
-let platillo = new Platillo(document, 100);
+let objetos = [];
+
+setInterval(
+    function()
+    {
+        let nuevoPlatillo = new Platillo(document, Math.random() * 50);
+        objetos.push(nuevoPlatillo);
+    },
+    2000
+);
 
 function step(delta)
 {
-    platillo.update(delta);
+    for (let index = 0; index < objetos.length; index++)
+    {
+        let objeto = objetos[index];
+        objeto.update(delta);
+    }
     requestAnimationFrame(step);
 }
 
